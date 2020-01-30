0f74866f21f2d81ac441513573f277c2407f11e5
fieldyang <fieldyang@163.com>
2020-01-30 23:48:13 +0800

    factory

--- core/expressionfactory.ts
+++ core/expressionfactory.ts
@@ -1,5 +1,7 @@
-/**
- * 表达式工厂
- */
-class ExpressionFactory extends Factory{
+namespace nodom {
+
+    /**
+     * 表达式工厂
+     */
+    class ExpressionFactory extends Factory {}
 }
--- core/factory.ts
+++ core/factory.ts
@@ -1,35 +1,51 @@
-/**
- * 工厂基类
- */
-class Factory{
-	constructor(module){
-		if(module !== undefined){
-			this.moduleName = module.name;
-		}
-		//容器map
-		this.items = Object.create(null);
-	}
-
+namespace nodom {
 	/**
-	 * 添加到工厂
-	 */
-	add(name,item){
-		this.items[name] = item;
-	}
+     * 工厂基类
+     */
+    export class Factory {
+		/**
+		 * 模块名
+		 */
+		moduleName:string;
+		/**
+		 * 工厂item对象
+		 */
+		items:object;
+		
+		/**
+		 * @param module 模块
+		 */
+		constructor(module?:Module) {
+            if (module !== undefined) {
+                this.moduleName = module.name;
+            }
+            //容器map
+            this.items = Object.create(null);
+        }
 
-	/**
-	 * 获得item
-	 */
-	get(name){
-		return this.items[name];
-	}
+        /**
+         * 添加到工厂
+		 * @param name 	item name
+		 * @param item	item
+         */
+        add(name:string, item:any) {
+            this.items[name] = item;
+        }
 
- 	/**
- 	 * 从容器移除
- 	 */
-	remove(name){
-		delete this.items[name];
-	}
+        /**
+         * 获得item
+		 * @param name 	item name
+         */
+        get(name:string) {
+            return this.items[name];
+        }
 
-	
+        /**
+         * 从容器移除
+		 * @param name 	item name
+         */
+        remove(name) {
+            delete this.items[name];
+        }
+    }
 }
